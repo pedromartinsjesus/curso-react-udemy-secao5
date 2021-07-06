023fc100a55a496edaf4cbff03e27ed776574e3e
Pedro <pedromjesusti@gmail.com>
2021-07-06 20:03:22 -0300

    All done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+
+  const [fnumber, setFnumber] = useState();
+  const [snumber, setSnumber] = useState();
+  const [result, setResult] = useState();
+
+
+  const somar = () => {
+
+    let resultAdd = parseInt(fnumber) + parseInt(snumber);
+
+    setResult(resultAdd);
+  }
+
+
   return (
-    <div className="App">
+    <div>
+      FNumber:  <input type="number" value={fnumber} onChange={(e) => setFnumber(e.target.value)} />
+      <br />
+
+     SNumber: <input type="number" value={snumber} onChange={(e) => setSnumber(e.target.value)} />
+      <br />
+
+     Result:  <input type="number" value={result} readOnly />
+
+      <br />
+
+      <button onClick={somar}>ADD</button>
+
 
     </div>
   );
